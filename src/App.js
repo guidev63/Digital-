8c183ed387c3bcde32738b55c6b4a9bc77eee718
Guilherme Augusto <guilhermedev23@gmail.com>
2024-08-21 18:48:42 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React from 'react'
-import './app.css'
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
-import Main from './components/Main/Main';
-import Footer from './components/Footer/Footer';
+import React from "react";
+import Navbar from "./component/Navbar";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from "./component/Home"; 
+import About from "./component/About"; 
+import Skills from "./component/Skills"; 
+import Services from "./component/Services"; 
+import Contact from "./component/Contact"; 
 
-const App = () =>{
-    return(
-        <>
-        <Navbar/>
-       {/* <Home/>
-        <Main/>
-        <Footer />   */}
-        </>
-    )
-}
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          {/* Rota para o componente Home */}
+          <Route path='/' exact component={Home} />
+          <Route path='/about' exact component={About} />
+          <Route path='/skills' exact component={Skills} />
+          <Route path='/services' exact component={Services} />
+          <Route path='/contact' exact component={Contact} />
+        </Switch>
+      </Router>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/component/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <section className='hero'>
+            <h1>Welcome To</h1>
+        </section>
+    );
+};
+
+export default Home;
