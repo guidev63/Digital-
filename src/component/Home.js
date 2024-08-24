d0aacda76dd87fe7bcc7c47b988d51d468338b2e
Guilherme Augusto <guilhermedev23@gmail.com>
2024-08-24 02:41:20 -0300

    logo colocada

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
     return (
         <section className='hero'>
-            <h1>Welcome To</h1>
+            <h1></h1>
         </section>
     );
 };
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "./navbar.css";
 import { FaBars } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
+import logo from "../assets/Logo.png"; // Ajuste o caminho conforme necessário Sua Logo.
 
 const Navbar = () => {
   const [Mobile, setMobile] = useState(false);
@@ -10,16 +11,36 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="container">
-        <h3 className="logo"></h3>
-        <ul className= {Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}>
-          <Link to="/"> <li>Home</li></Link>
-          <Link to="/about"> <li>About</li></Link>
-          <Link to="/services"> <li>Services</li></Link>
-          <Link to="/skills"> <li>Skills</li></Link>
-          <Link to="/contact"> <li>Contact</li></Link>
+        <h3 className="logo">
+          <img src={logo} alt="Logo"/>
+        </h3>
+        <ul
+          className={Mobile ? "nav-links-mobile" : "nav-links"}
+          onClick={() => setMobile(false)}
+        >
+          <Link to="/">
+            {" "}
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            {" "}
+            <li>About</li>
+          </Link>
+          <Link to="/services">
+            {" "}
+            <li>Services</li>
+          </Link>
+          <Link to="/skills">
+            {" "}
+            <li>Skills</li>
+          </Link>
+          <Link to="/contact">
+            {" "}
+            <li>Contact</li>
+          </Link>
         </ul>
         <button className="mobile-menu-icon" onClick={() => setMobile(!Mobile)}>
-          {Mobile ? <ImCross /> : <FaBars /> }
+          {Mobile ? <ImCross /> : <FaBars />}
         </button>
       </div>
     </nav>
